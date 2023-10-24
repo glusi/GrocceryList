44575291e1723bad1e1f9d5517eb1a71df388ae8
simonq <g@>
2023-10-24 16:01:40 +0300

    added images

--- app/src/main/java/com/example/groccery/ListGen.kt
+++ app/src/main/java/com/example/groccery/ListGen.kt
@@ -2,10 +2,13 @@ package com.example.groccery
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.GridView
 import android.widget.ListAdapter
 import android.widget.ListView
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import java.io.Serializable
 
@@ -22,9 +25,29 @@ class ListGen : AppCompatActivity() {
         var position = bundle!!.getInt("position")
         print(position)
 
+        var titles : ArrayList<String> = MainActivity.titles
+        val title: TextView = findViewById(R.id.title)
+        title.text = titles[position]
+
         var lists : ArrayList<List<String>> = MainActivity.lists
         listView = findViewById(R.id.list_products)
         listView.adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, lists[position])
+
+        listView.onItemClickListener = AdapterView.OnItemClickListener { _, view, position_iternal, _ ->
+            Toast.makeText(
+                applicationContext,  " selected",
+                Toast.LENGTH_SHORT
+            ).show()
+            if(view.alpha!=0.5f) {
+                view.alpha = 0.5f
+            }
+            else{
+                view.alpha = 1.0f
+            }
+        }
+
+//        setSupportActionBar(findViewById(R.id.my_toolbar))
+//        title = findViewById(R.id.my_toolbar)
 
         position += 1
         val fab: View = findViewById(R.id.next)
