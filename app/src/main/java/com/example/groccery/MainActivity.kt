da22707f0a796d153f6f5da73354b79be67e6c0d
simonq <g@>
2023-10-25 18:37:14 +0300

    a little customization

--- app/src/main/java/com/example/groccery/MainActivity.kt
+++ app/src/main/java/com/example/groccery/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.groccery
 
 import android.content.Intent
+import android.graphics.Shader
+import android.graphics.drawable.BitmapDrawable
 import android.os.Bundle
 import android.view.View
 import android.widget.AdapterView
@@ -18,7 +20,17 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val back_main : View = findViewById(R.id.back_main)
+        val resource: BitmapDrawable =
+            getResources().getDrawable(R.drawable.back_main) as BitmapDrawable
+        resource.setTileModeXY(Shader.TileMode.REPEAT, Shader.TileMode.REPEAT)
+        back_main.setBackground(resource)
+        val background = back_main.background
+        background.alpha = 50
+
         val button: Button = findViewById(R.id.button)
+        button.getBackground().setAlpha(90)
         button.setOnClickListener {
             val intent = Intent(this, MainChoose::class.java)
             startActivity(intent)
